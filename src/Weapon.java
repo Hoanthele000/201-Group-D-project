383ca18d65cef5d295e64e15059b094d98c7511d
Lovellmb <163305664+Lovellmb@users.noreply.github.com>
2024-10-31 17:24:54 -0400

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,4 +1,23 @@
 
 public class Weapon {
-
+ 	int power;
+	
+	/**
+	 * Constructor for weapon class.
+	 * @param name the weapon's name
+	 * @param power the weapon's power
+	 */
+	Weapon(String name, int power){
+		super(name);
+		this.power = power;
+		
+	}
+	
+	/**
+	 * Returns the weapon's power
+	 * @return power
+	 */
+	public int getPower() {
+		return power;
+	}
 }
