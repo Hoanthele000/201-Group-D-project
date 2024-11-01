e002c0916b7da6429b204bc6cb2d11ae81abedfd
Hoanthele000 <hoanle200409@gmail.com>
2024-11-01 11:24:12 -0400

    Update Witch.java

--- src/Witch.java
+++ src/Witch.java
@@ -1,10 +1,21 @@
+/**
+ * Class: Witch
+ * Written: 1 Nov 2024
+ * 
+ * Purpose: Implementation of Witch monster
+ */
 public class Witch extends Enemy{
 	
+	// Heal variable for Witch
 	int heal = 2;
 	
+	// Constructor for Witch class
 	public Witch(String type) {
 		super(type);
 	
+	/**
+	 * Method to activate spell for Witch
+	 */
 	public void spell() {
 		this.health += 2;
 	}
