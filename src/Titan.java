6569645437bfdcaeeaf96c727dc28654e0e88e6d
Hoanthele000 <hoanle200409@gmail.com>
2024-11-01 11:23:41 -0400

    Update Titan.java

--- src/Titan.java
+++ src/Titan.java
@@ -1,5 +1,12 @@
+/**
+ * Class: Titan
+ * Written: 1 Nov 2024
+ * 
+ * Purpose: Implementation of Titan monster
+ */
 public class Titan extends Enemy{
 	
+	// Constructor of Titan class
 	public Titan(String type) {
 		this.type = type;
 		this.health = 30;
