9922176562fbe2774b437c404236815c5e73aefc
Lovellmb <163305664+Lovellmb@users.noreply.github.com>
2024-11-01 14:40:19 -0400

    Update Room.java
    Changed name of default monster from "bob" to "monster"
    Added javadoc for the class

--- src/Room.java
+++ src/Room.java
@@ -1,9 +1,17 @@
 
+/**
+* Class: Room
+* @author Group D
+* @version 1.0
+* Course : CSE 201 Fall 2024
+*
+* Purpose: Creates a room object to represent a room that contains a challenge. Once cleared, the challenge will no longer apear in the room.
+*/
 public class Room {
 	
-	Enemy enemy;	
+	Enemy enemy; // the enemy in the room, if any
 	String type; // healing, enemy, boss, could add more.
-	String clear;
+	String clear; // shows a symbol based on whether the user has cleared the room.
 	
 	/**
 	 * Creates a room object
@@ -12,7 +20,7 @@ public class Room {
 	Room(String type){
 		this.type = type;
 		if (type.equals("monster")){
-			this.enemy = new Enemy("bob"); // should be altered when enemy class is finished
+			this.enemy = new Enemy("monster"); // should be altered when enemy class is finished
 		}
 		if (type.equals("boss")){
 			this.enemy = new Enemy("Zargotrax, master of darkness"); // also to be altered with enemy class. should be a set boss
