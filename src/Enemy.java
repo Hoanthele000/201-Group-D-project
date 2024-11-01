8f1bf4aaff3a3e7ea4d2be7a9167080ba636a708
Hoanthele000 <hoanle200409@gmail.com>
2024-11-01 11:23:18 -0400

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,17 +1,32 @@
-
+/**
+ * Class: Enemy
+ * Written: 1 Nov 2024
+ * 
+ * Purpose: Implementation of Enemy
+ */
 public class Enemy {
+	
+		// Variable for Enemy class
         String type;
         int health;
 
+        // Constructor for Enemy class
         public Enemy(String type) {
             this.type = type;
             this.health = 20; // Default health for all enemies
         }
 
+        /**
+         * Method to print the type and health of Enemy
+         */
         public void displayStats() {
             System.out.println(type + " HP: " + health);
         }
         
+        /**
+         * Method for enemy to attack the player
+         * @param player for enemy to attack
+         */
         public void enemyAttacks(Player player) {
             if (player.dodgeAttack()) {
                 System.out.println("You dodged the attack!");
