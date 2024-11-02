af1a8042873165a3022a7874f831da211311a231
Lovellmb <163305664+Lovellmb@users.noreply.github.com>
2024-11-02 18:10:25 -0400

    Update Item.java
    Added javadoc

--- src/Item.java
+++ src/Item.java
@@ -1,14 +1,30 @@
 
+/**
+* Class: Item
+* @author Group D
+* @version 1.0
+* Course : CSE 201 Fall 2024
+*
+* Purpose: Represents a consumable item or weapon that the player stores in their inventory.
+*/
 public class Item {
 	
-	private String name;
 	
+	private String name; // the name of the item
+	
+	/**
+	 * Constructor that sets the item's name
+	 * @param name
+	 */
 	Item(String name){
 		this.name = name;
 	}
 
+	
+	/**
+	 * Getter method for the item name
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
-	
-}
