8145de49dfa452ce5e5564f06f85ad8dcb67fa5c
Lovellmb <163305664+Lovellmb@users.noreply.github.com>
2024-11-02 12:14:32 -0400

    Update Game.java
    created helper method for battle to shorten to 30 lines.
    moved storephase() method to store class and split into multiple methods. Also changed the storephase call to function with new class

--- src/Game.java
+++ src/Game.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 import java.util.Random;
 
 /**
- * Primary class that runs the game
- */
+* Class: Game
+* @author Group D
+* @version 1.0
+* Course : CSE 201 Fall 2024
+*
+* Purpose: Starts and runs the game.
+*/
 public class Game {
     
     /**
@@ -13,7 +18,7 @@ public class Game {
     static void playGame(Scanner scanner) {
         Dungeon dungeon = new Dungeon();
         boolean gameIsOver = false;       
-            
+	    
         System.out.println("Please enter your character name: "); // Character Creation
         String characterName = scanner.nextLine();
         System.out.println("Please select your character's race: " +
@@ -25,14 +30,13 @@ public class Game {
             System.out.println("Not a valid input, please try again..."); 
             characterRace = scanner.nextLine();
         }
-        Player player = new Player(characterName, characterRace);
-        
-        storePhase(player, scanner); // Shopping phase before the dungeon
+        Player player = new Player(characterName, characterRace);        
+        Store store = new Store();
+        store.storePhase(player, scanner); 
         // Game loop
         do {           
             dungeon.printDungeon(); // Display the dungeon to the user and marks their location            
             gameIsOver = dungeon.move(scanner); // Allow player to move
-
              if (dungeon.getRoom().getType().equals("monster") && !gameIsOver && !dungeon.getRoom().isCleared()) { 
                 Enemy enemy = dungeon.getRoom().getEnemy();
                 battle(player, enemy, scanner);
@@ -42,132 +46,56 @@ public class Game {
         scanner.close();
     }
 
-    // NEEDS TO BE MUCH SHORTER, i recomend making a store class to be used from pleyGame(), and splitting this into several methods
-    static void storePhase(Player player, Scanner scanner) {
-        // Maybe add a random quality dropper dictated with stars to indicate quality. Bosses?
-        // Maybe include a score generator after each monster kill
-        boolean keepGoing = true;
-
-        do {
-            System.out.println("***********Store***************\n");
-            System.out.println("Gold: " + player.getGold());
-            System.out.println("Purchase: \"Health Potion\" (50 Gold)");
-            System.out.println("Purchase: \"Mana Potion\" (50 Gold)");
-            System.out.println("Purchase: \"Basic GreatSword\" (500 Gold)");
-            System.out.println("Purchase: \"Basic Longsword\" (500 Gold)");
-            System.out.println("Purchase: \"Basic Longbow\" (500 Gold)");
-            System.out.println("Purchase: \"Basic Staff\" (500 Gold)");
-            System.out.println("**********************************");
-            System.out.println("Enter your purchase: ");
-            String choice = scanner.nextLine().toLowerCase();
-
-            switch (choice) {
-                case "health potion":
-                case "mana potion":
-                    System.out.println("How many would you like to buy?");
-                    int quantity = Integer.parseInt(scanner.nextLine());
-                    int totalCost = quantity * 50;
-                    if (player.spendGold(totalCost)) {
-                        System.out.println("You purchased " + quantity + " " + choice + "(s).");
-                        for (int i = 0; i < quantity; i++) {
-                        	player.addItem(new Item("potion"));
-                        }
-                        System.out.println("Would you like to purchase anything else?\n\"Yes\"\n\"No \"");
-                        
-                        boolean valid = true;
-                        do {
-                            String choice2 = scanner.nextLine();                           
-                            switch (choice2.toLowerCase()) {
-                                case "yes":
-                                    keepGoing = true;
-                                    break;
-                                case "no":
-                                    keepGoing = false;
-                                    break;
-                                default:
-                                    System.out.println("Invalid input...");
-                                    valid = false;
-                                    break;
-                            } 
-                        } while (valid == false);
-                    }
-                    break;
-                case "basic greatsword":
-                case "basic longsword":
-                case "basic longbow":
-                case "basic staff":
-                    if (player.spendGold(500)) {
-                        System.out.println("You have purchased a " + choice + ".");
-                        player.addItem(new Weapon(choice, 5));
-                        System.out.println("Would you like to equip it? (yes/no)");
-                        String equipChoice = scanner.nextLine().toLowerCase();
-                        if (equipChoice.equals("yes")) {
-                            player.equipWeapon(new Weapon(choice,5));
-                            System.out.println("You equipped the " + choice + ".");
-                        }
-                    }
-                    System.out.println("Would you like to purchase anything else?\n \" Yes\" \n \" No \"");
-                    String choice2 = scanner.nextLine();
-                    switch (choice2.toLowerCase()) {
-                        case "yes":
-                            keepGoing = true;
-                            break;
-                        case "no":
-                            keepGoing = false;
-                            break;
-                        default:
-                            System.out.println("Invalid input...");
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid choice, please try again.");
-                    break;
-            }
-        } while (keepGoing != false);
+    
+    
+    /**
+     * Prints the information for a battle.
+     * @param player the user's character
+     * @param enemy the enemy to battle
+     */
+    static void printBattleInfo(Player player, Enemy enemy) {
+    	// Display enemy and player stats
+        System.out.println("*******You have encountered an enemy!**********");
+        enemy.displayStats();
+        System.out.println("***************************************");
+        player.displayStats();
+        System.out.println("Choose action: Attack, Flee, Check Inventory");
     }
     
-    // too long, needs to be split into methods   
+    
+    /**
+     * Allows a player to attack, flee or use items during battle.
+     * @param player the user's character
+     * @param enemy the enemy to battle
+     * @param scanner takes user inputs
+     */
     static void battle(Player player, Enemy enemy, Scanner scanner) {
         boolean battleOver = false;
-
         while (!battleOver) {
-            // Display enemy and player stats
-            System.out.println("*******You have encountered an enemy!**********");
-            enemy.displayStats();
-            System.out.println("***************************************");
-            player.displayStats();
-
-            System.out.println("Choose action: Attack, Flee, Check Inventory");
+            printBattleInfo(player,enemy);
             String action = scanner.nextLine().toLowerCase();
-
             switch (action) {
                 case "attack":
-                    // Player attacks enemy
-                    System.out.println("You attack the " + enemy.type + " for " + player.calculateDamage() + " damage!");
-                    enemy.health -= player.calculateDamage();
-                    if (enemy.health <= 0) {
-                        System.out.println("You defeated the " + enemy.type + "!" + " It dropped 100 gold!");
-                        player.addGold(100);
-                        // player.addItem(); // todo : implement method
-                        battleOver = true;
-                    } else {
-                         enemy.enemyAttacks(player);
-                    }
-                    break;
+		    	System.out.println("You attack the " + enemy.type + " for " + player.calculateDamage() + " damage!");                      
+                    	if ((enemy.health -= player.calculateDamage()) <= 0) {
+                    		System.out.println("You defeated the " + enemy.type + "!" + " It dropped 100 gold!");
+                    		player.addGold(100); // player.addItem(); // todo : implement method
+                    		battleOver = true;
+                	} else {
+                    		enemy.enemyAttacks(player);
+                	}
+                	break;
                 case "flee":
-                    battleOver = player.flee();
-                    if (!battleOver) {
-                        enemy.enemyAttacks(player);
-                    }
-                    break;
+                	battleOver = player.flee(enemy);
+                        break;
                 case "check inventory":
-                    System.out.println("Your inventory contains: ");
-                    player.printInventory();
-                    break;
-                default:
-                    System.out.println("Invalid action. Please choose again.");
-                    break
-            }
+                        System.out.println("Your inventory contains: ");
+                        player.printInventory();
+                        break;
+                 default:
+                        System.out.println("Invalid action. Please choose again.");
+                break;
+	    }
         }
     }
 
