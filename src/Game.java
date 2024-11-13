8e1ef504dd49e62b8fbc4e20a376f21fb8484522
andrewstilge <stilgeat@miamioh.edu>
2024-11-13 11:17:28 -0500

    Added simple high score

--- src/Game.java
+++ src/Game.java
@@ -1,130 +1,279 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
-* Class: Game
+* Class: Player
 * @author Group D
 * @version 1.0
 * Course : CSE 201 Fall 2024
 *
-* Purpose: Starts and runs the game.
+* Purpose: Creates a player object that represents the user's character in the game.
+* It stores the character's stats, gold, and items/inventory.
 */
-public class Game {
-    
+public class Player {
+	
+    String name; // player's name
+    private int strength, vitality, intelligence, dexterity; // various stats to affect game actions
+    private int health; // the player's health
+    private int maxHealth; // the maximum health the player can hav
+    private Weapon equippedWeapon; // the wepon being used
+    private int gold; 
+    private Item[] inventory;
+    private int highScore = 0; // Player high score
+
     /**
-     * Starts and runs the game.
-     * @param scanner takes user inputs
+     * Constructor for a player object, takes a name and race to initialize stats.
+     * @param name -the player's name
+     * @param race -a parameter that affect starting stats.
      */
-    static void playGame(Scanner scanner) {
-        Dungeon dungeon = new Dungeon();
-        boolean gameIsOver = false;       
-	    
-        System.out.println("Please enter your character name: "); // Character Creation
-        String characterName = scanner.nextLine();
-        System.out.println("Please select your character's race: " +
-                "Barbarian (+10 STR), Wizard (+10 INT), Warrior (+10 VIT), Ranger (+10 DEX)");
-        String characterRace = scanner.nextLine();
+    public Player(String name, String race) {
+        this.name = name;
+        this.gold = 1000;
+        this.inventory = new Item[15];
+        initializeStats(race);
+    }
         
-        while (!(characterRace.equalsIgnoreCase("barbarian") || characterRace.equalsIgnoreCase("wizard")
-                || characterRace.equalsIgnoreCase("warrior") || characterRace.equalsIgnoreCase("ranger"))) {
-            System.out.println("Not a valid input, please try again..."); 
-            characterRace = scanner.nextLine();
+    /**
+     * Damages the player by reducing their health and checks for a game over. 
+     * @param damage -the amount of damage to be dealt to the player
+     */
+    public void damage(int damage) {
+        health -= damage;
+        if (health <= 0) {
+    		System.out.println("You have been defeated!");
+            gameOver();
         }
-        Player player = new Player(characterName, characterRace);        
-        Store store = new Store();
-        store.storePhase(player, scanner); 
-        // Game loop
-        do {           
-                dungeon.printDungeon(); // Display the dungeon to the user and marks their location            
-                gameIsOver = dungeon.move(scanner); // Allow player to move
-                dungeon.getRoom.enter();
-		betweenRooms(scanner, player);
-        } while (!gameIsOver);
-        scanner.close();
     }
 
-  static void betweenRooms(Scanner scanner, Player player) {
-    	
-    	String input;
-    	 do {
-    		System.out.println("enter inventory to use inventory, stats to view your status, or move to continue to the next room");
-    		input = scanner.nextLine().toLowerCase();
-    		switch (input) {
-    			case "inventory":
-    				player.printInventory(scanner);
-    				break;
-    			case "stats":
-    				player.displayStats();
-    				break;
-    			case "move":
-    				break;
-    			default:
-    				System.out.println("invalid input, try again");
-    				break;
-    		}
-    	
-    	} while (!input.equals("move"));
-    }
 
-    
-    /**
-     * Prints the information for a battle.
-     * @param player the user's character
-     * @param enemy the enemy to battle
+     /**
+     * Prints the contents of player's inventory and allows player to use an item
      */
-    static void printBattleInfo(Player player, Enemy enemy) {
-    	// Display enemy and player stats
-        System.out.println("*******You have encountered an enemy!**********");
-        enemy.displayStats();
-        System.out.println("***************************************");
-        player.displayStats();
-        System.out.println("Choose action: Attack, Flee, Check Inventory");
+    public void printInventory(Scanner scanner) {
+    	for (int i = 0; i < inventory.length; i++) {
+        	if (inventory[i] != null) {
+        		System.out.print("slot "+ i + ": [  " + inventory[i].getName() + "  ]");
+    			System.out.println();
+    		} else {
+				System.out.print("slot "+ i + ": [    ]");
+        		System.out.println();
+        	}      		
+        }
+        System.out.println("Enter the slot number of the item you would like to equip");
+        int choice = Integer.parseInt(scanner.nextLine());
+        if (inventory[choice] != null) {
+        	useItem(choice);
+        }
     }
-    
-    
-    /**
-     * Allows a player to attack, flee or use items during battle.
-     * @param player the user's character
-     * @param enemy the enemy to battle
-     * @param scanner takes user inputs
+	
+    /** 
+     * Adds an item to the player inventory if there is an open slot.
+     * Otherwise, tells the player that the inventory is full
+     * @param item -the item to be added to the inventory.
      */
-    static void battle(Player player, Enemy enemy, Scanner scanner) {
-        boolean battleOver = false;
-        while (!battleOver) {
-            printBattleInfo(player,enemy);
-            String action = scanner.nextLine().toLowerCase();
-            switch (action) {
-                case "attack":
-		    	System.out.println("You attack the " + enemy.type + " for " + player.calculateDamage() + " damage!");                      
-                    	if ((enemy.health -= player.calculateDamage()) <= 0) {
-                    		System.out.println("You defeated the " + enemy.type + "!" + " It dropped 100 gold and a health potion!");
-                    		player.addGold(100); 
-				player.addItem(new Item("health potion"));
-                    		battleOver = true;
-                	} else {
-                    		enemy.enemyAttacks(player);
-                	}
-                	break;
-                case "flee":
-                	battleOver = player.flee(enemy);
-                        break;
-                case "check inventory":
-                        System.out.println("Your inventory contains: ");
-                        player.printInventory(scanner);
-                        break;
-                 default:
-                        System.out.println("Invalid action. Please choose again.");
-                break;
+    public void addItem(Item item) {
+        boolean full = true;
+        for (int i = 0; i < inventory.length; i++) {
+        	if (inventory[i] == null && full) {
+        		inventory[i] = item;
+        		full = false;
+        	} 
+        }
+        if (full) {
+        	System.out.println("Inventory full, unable to add item.");
+        }
+    }
+        
+    /**
+     * Consumes potion or equips weapon in the given slot.
+     * @param index - the posistion of the item to be used.
+     */
+    public void useItem(int index) {
+        if (inventory[index].getName().equals("health potion")) {
+    	    heal(10);
+            inventory[index] = null;
+        }
+        if (inventory[index] instanceof Weapon) {
+            equipWeapon((Weapon) inventory[index]);
+        }        	
+    }
+        
+        
+    /**
+     * Heals the player to no more than maxHealth
+     * @param amount - the amount to be healed
+     */
+    public void heal(int amount) {
+	    if (amount + health >= maxHealth) {
+		    health = maxHealth;
+	    } else {
+		    health += amount;
 	    }
+	}
+
+    /**
+     * Allows the player to spend gold
+     * @param amount the amount to spend
+     * @return a success or fail message in accordance to the result.
+     */
+	public boolean spendGold(int amount) {
+        if (gold >= amount) {
+        	gold -= amount;
+            System.out.println("You spent " + amount + " gold. Remaining gold: " + gold);
+        	return true;
+        } else {
+            System.out.println("You don't have enough gold!");
+            return false;
         }
+    }
+        
+	/**
+	 * Equips a weapon 
+	 * @param weapon the weapon to equip
+	 */
+    public void equipWeapon(Weapon weapon) {
+        this.equippedWeapon = weapon;
+    }
+        
+    /**
+     * Displays relevant player information
+     */
+    public void displayStats() {
+        System.out.println("Stats: ");
+    	System.out.println("Health: " + health + "/" + maxHealth);
+       	System.out.println("Strength: " + strength);
+        System.out.println("Intelligence: " + intelligence);
+    	System.out.println("Dexterity: " + dexterity);
+    	System.out.println("Equipped Weapon: " + equippedWeapon.getName());
+      	System.out.println("Weapon Damage: " + equippedWeapon.getPower());
+       	System.out.println("Gold: " + gold);
     }
 
     /**
-	 *  Calls playGame() to begin the game.
-	 * @param args command line arguments
-	 */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        playGame(scanner);
-    }  
+     * Adds to the player's high score when an enemy is killed or a task is completed.
+     * @param points - the points to be added to the high score
+     */
+    public void addHighScore(int points) {
+        highScore += points;
+        System.out.println("High Score increased by " + points + ". Current High Score: " + highScore);
+    }
+
+    /**
+     * Returns the high score the player has
+     * @return the player's point total
+     */
+    public int getScore() {
+        return highScore;
+    }
+
+        
+    /**
+     * Determines how much damage the player will deal.
+     * @return the damage to deal
+     */
+    public int calculateDamage() {
+    	if (equippedWeapon != null) {
+    		return equippedWeapon.getPower() + strength;
+    	}
+    	return strength;
+    }
+        
+    /**
+     * Ends the game and prints a loss message.
+     */
+    public void gameOver() {
+        System.out.println("Your HP has reached 0. Game Over.");
+    	System.exit(0); // Terminate the game
+    }
+        
+        
+    /**
+     * allows player to dodge incoming attacks
+     * @return
+     */
+    public boolean dodgeAttack() {
+        Random rand = new Random();
+        return rand.nextInt(100) < dexterity; // Dexterity % chance to dodge
+    }
+
+    /**
+     * Adds gold to the player
+     * @param amount -the amount to add
+     */
+    public void addGold(int amount) {
+        gold += amount;
+        System.out.println("You earned " + amount + " gold. Current gold: " + gold);
+    }
+        
+    /**
+     * Allows player to flee from combat
+     * @return true if successful, false otherwise
+     */
+    public boolean flee(Enemy enemy) { 	
+        Random random = new Random();          
+        if (random.nextInt(12) + dexterity >= 6) {
+           System.out.println("You successfully fled the battle!");
+           return true;
+        } else {
+            System.out.println("Failed to flee! You've been counterattacked!");
+            enemy.enemyAttacks(this);
+            return false;
+        }
+	}
+        
+    /**
+     * Intializes the players stats based on their race.
+     */
+    private void initializeStats(String race) {
+        switch (race.toLowerCase()) {
+            case "barbarian":
+                strength = 10;
+                vitality = 5;
+                intelligence = 1;
+                dexterity = 2;
+                break;
+            case "wizard":
+                strength = 1;
+                vitality = 3;
+                intelligence = 10;
+                dexterity = 2;
+                break;
+            case "warrior":
+                strength = 5;
+                vitality = 10;
+                intelligence = 2;
+                dexterity = 2;
+                break;
+            case "ranger":
+                strength = 2;
+                vitality = 3;
+                intelligence = 3;
+                dexterity = 10;
+                break;
+            default:
+                strength = 5;
+                vitality = 5;
+                intelligence = 5;
+                dexterity = 5;
+                break;
+        }
+        maxHealth = 10 + vitality;
+        health = maxHealth;
+    }
+        
+    /**
+     * Returns the amount of gold the player has
+     * @return the player's gold
+     */
+	public int getGold() {
+		return gold;
+	}
+	
+	/**
+	* Returns the current player health
+	* @return health
+	*/
+	public int getHealth() {
+		return health;
+	}
 }
