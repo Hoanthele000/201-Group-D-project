537a420973ddb40c5addb301994371322535f36d
Lovellmb <163305664+Lovellmb@users.noreply.github.com>
2024-11-27 15:08:57 -0500

    Update MonsterRoom.java
    javadoc

--- src/MonsterRoom.java
+++ src/MonsterRoom.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
 
+
+/**
+ * Class: MonsterRoom
+ * @author Group D
+ * @version 1.0
+ * Course : CSE 201 Fall 2024
+ * 
+ * Purpose: Represents a room full of monsters for the player to fight.
+ */
 public class MonsterRoom extends Room{
 
+	/**
+	 * Monster Room constructor
+	 */
 	MonsterRoom(){
 		super("monster");
 	}
 	
+	
+	/**
+	 * Handles room operations.
+	 */
 	public void enter(Player player, Scanner scanner) {
 		if (isCleared()) {
 			System.out.println("As you enter the empty room, you cant help but feel a sense of deja vu");
